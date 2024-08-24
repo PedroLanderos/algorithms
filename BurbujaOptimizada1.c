c8ca1ba3b882dec33376f8ad5891a47f65441ad6
Muxutruk2 <156070698+Muxutruk2@users.noreply.github.com>
2024-08-24 16:38:58 +0200

    Simple formatting

--- BurbujaOptimizada1.c
+++ BurbujaOptimizada1.c
@@ -1,95 +1,95 @@
-#include<stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include<stdio.h>                                                        
+#include <stdlib.h>                                                      
+#include <time.h>                                                        
 
-void Rellenar(int *vector, int size, const char *nombreArchivo)
+void Rellenar(int *vector, int size, const char *nombreArchivo          )
 {
-    FILE *archivo = fopen(nombreArchivo, "r");
+    FILE *archivo = fopen(nombreArchivo, "r"                           );
 
-    if (archivo == NULL)
+    if (archivo == NULL                                                 )
     {
-        printf("No se pudo abrir el archivo.\n");
-        return;
+        printf("No se pudo abrir el archivo.\n"                        );
+        return                                                          ;
     }
 
-    int i;
-    for (i = 0; i < size; i++)
+    int i                                                               ;
+    for (i = 0; i < size; i++                                           )
     {
-        if (fscanf(archivo, "%d", &vector[i]) != 1)
+        if (fscanf(archivo, "%d", &vector[i]) != 1                      )
         {
-            printf("Error al leer el archivo.\n");
-            break;
+            printf("Error al leer el archivo.\n"                       );
+            break                                                       ;
         }
     }
 
-    fclose(archivo);
+    fclose(archivo                                                     );
 }
 
-int ValidarN(const char *nombreArchivo)
+int ValidarN(const char *nombreArchivo                                  )
 {
-    FILE *archivo = fopen(nombreArchivo, "r");
+    FILE *archivo = fopen(nombreArchivo, "r"                           );
 
-    int contador = 0;
-    int valor;
+    int contador = 0                                                    ;
+    int valor                                                           ;
 
-    while (fscanf(archivo, "%d", &valor) == 1)
+    while (fscanf(archivo, "%d", &valor) == 1                           )
     {
-        contador++;
+        contador++                                                      ;
     }
 
-    fclose(archivo);
+    fclose(archivo                                                     );
 
-    return contador;
+    return contador                                                     ;
 }
 
-void Burbuja(int *vector, int n)
+void Burbuja(int *vector, int n                                         )
 {
-    int i, j, aux;
-    for(i = 0; i < n; i++)
+    int i, j, aux                                                       ;
+    for(i = 0; i < n; i++                                               )
     {
-        for(j = 0; j < n; j++)
+        for(j = 0; j < n; j++                                           )
         {
-            if(*(vector + j) > *(vector + (j + 1)))
+            if(*(vector + j) > *(vector + (j + 1                      )))
             {
-                aux = *(vector + j);
-                *(vector + j) = *(vector + (j + 1));
-                *(vector + (j + 1)) = aux;
+                aux = *(vector + j                                     );
+                *(vector + j) = *(vector + (j + 1                     ));
+                *(vector + (j + 1)) = aux                               ;
             }
         }
     }
 }
 
-void Imprimir(int *vector, int n)
+void Imprimir(int *vector, int n                                        )
 {
-    int i;
-    for(i  = 0; i < n; i++)
+    int i                                                               ;
+    for(i  = 0; i < n; i++                                              )
     {
-        printf("%i ", *(vector + i));
+        printf("%i ", *(vector + i                                    ));
     }
 
-    printf("\n\n");
+    printf("\n\n"                                                      );
 }
 
 
-int main(int argc, char const *argv[])
+int main(int argc, char const *argv[]                                   )
 {
-    int *vector;
-    int n, nValida;
-    scanf("%i", &n);
+    int *vector                                                         ;
+    int n, nValida                                                      ;
+    scanf("%i", &n                                                     );
 
-    vector = (int*)malloc(n*sizeof(int));
+    vector = (int*)malloc(n*sizeof(int                                ));
 
-	nValida =  ValidarN("NumerosPruebas.txt");
-    if(n > nValida)
-        printf("No hay suficientes elementos\n");
-    else
+	nValida =  ValidarN("NumerosPruebas.txt"                              );
+    if(n > nValida                                                      )
+        printf("No hay suficientes elementos\n"                        );
+    else                                                                 
     {
-        Rellenar(vector, n, "NumerosPruebas.txt");
-        Imprimir(vector, n);
-        Burbuja(vector, n);
-        Imprimir(vector, n);
+        Rellenar(vector, n, "NumerosPruebas.txt"                       );
+        Imprimir(vector, n                                             );
+        Burbuja(vector, n                                              );
+        Imprimir(vector, n                                             );
     }
     
 
-    return 0;
+    return 0                                                            ;
 }
